c88a1e648cf55f6da24f8ad0c658740f5f813857
Chason Chaffin <chason@gmail.com>
2018-03-31 11:57:30 +0900

    Adding bumpversion

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pytunes-reporter',
-    version='0.1',
+    version='0.1.0',
     description='Library to interact with iTunes Reporter API',
     long_description=long_description,
     url='https://github.com/gifbitjapan/pytunes-reporter',
